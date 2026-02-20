586c3459237e5d11a7b7ebaec6d05df1af8541ea
coder-nuralom <coder.nuralom@gmail.com>
2026-02-20 23:25:31 +0600

    change base url again

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -3,8 +3,7 @@ import React, { createContext, useContext, useState } from "react";
 const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-  const baseURL =
-    "https://mern-user-authentication-backend-production.up.railway.app";
+  const baseURL = "https://mern-user-authentication-backend.onrender.com";
   const [user, setUser] = useState(() => {
     const storedUser = localStorage.getItem("user");
     return storedUser ? JSON.parse(storedUser) : null;
